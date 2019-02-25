394bf19a4f96c16f596032e8ca31a52f413ef6f9
gitbootcamp <ahmad.gulzar8@gmail.com>
2019-02-26 00:07:32 +0530

    Update qus10.java

--- qus10.java
+++ qus10.java
@@ -1,74 +1,71 @@
-// Q7- Submit List of tasks to ExecutorService and wait for the completion of all the tasks.
-
-import java.awt.*;
-import java.util.ArrayList;
-import java.util.concurrent.*;
-
-class Submit_task_list
-{
-	public static void main(String[] args) throws InterruptedException {
-
-		ArrayList<Callable<Integer>> arrayList = new ArrayList<>();
-
-		arrayList.add(new Callable<Integer>() {
-			@Override
-			public Integer call() throws Exception {
-				return 1;
-			}
-		});
-
-		arrayList.add(new Callable<Integer>() {
-			@Override
-			public Integer call() throws Exception {
-				return 2;
-			}
-		});
-
-		arrayList.add(new Callable<Integer>() {
-			@Override
-			public Integer call() throws Exception {
-				return 3;
-			}
-		});
-
-		arrayList.add(new Callable<Integer>() {
-			@Override
-			public Integer call() throws Exception {
-				return 4;
-			}
-		});
-
-		ExecutorService executorService = Executors.newSingleThreadExecutor();
-		ArrayList<Future<Integer>> futureArrayList = (ArrayList<Future<Integer>>) executorService.invokeAll(arrayList);
-
-		futureArrayList.forEach((e)->{
-			if(e.isDone())
-			{
-				try {
-					System.out.println(e.get());
-				}
-				catch (ExecutionException el)
-				{
-					el.printStackTrace();
-				}
-				catch (InterruptedException e1)
-				{
-					e1.printStackTrace();
-				}
-			}
-
-		});
-
-		executorService.shutdown();
-		
-		executorService.awaitTermination(2000L,TimeUnit.MILLISECONDS);
-
-		if(executorService.isTerminated()){
-			System.out.println("Terminated");
-		}else{
-			System.out.println("One or more tasks still remaining");
-		}
-
+import java.util.*;
+public class qus10 {
+	//adding two integer number
+	int add(int a, int b)
+	{
+		return a+b;
 	}
+	//adding two double number
+	double add(double a, double b)
+	{
+		return a+b;
+	}
+	//multiplication of two float number
+	float multiply(float a, float b)
+	{
+		return a*b;
+	}
+	//multiplication of two int number
+	int multiply(int a, int b)
+	{
+		return a*b;
+	}
+	public static String concat(String s1, String s2)
+	{
+		return new StringBuilder(s1).append(s2).toString();
+	}
+	public static String concat(String s1, String s2, String s3)
+	{
+		return s1.concat(s2).concat(s3);
+	}
+}
+class qus10n {
+	public static void main (String[] args) {
 
+		qus10 ob = new qus10();
+		Scanner sc = new Scanner(System.in);
+
+
+		System.out.println("enter two integer numbers : ");
+
+
+
+
+
+
+
+		int sum1 = ob.add(sc.nextInt(),sc.nextInt());
+		System.out.println("sum of the two integer value :" + sum1);
+
+		System.out.println("enter two decimal numbers : ");
+		double sum2 = ob.add(sc.nextDouble(),sc.nextDouble());
+		System.out.println("sum of the three integer value :" + sum2);
+
+		System.out.println("enter two float numbers : ");
+		float mult1 = ob.multiply(sc.nextFloat(),sc.nextFloat());
+		System.out.println("multiplication of two float value:" + mult1);
+
+		System.out.println("enter two integer numbers");
+		int mult2 = ob.multiply(sc.nextInt(),sc.nextInt());
+		System.out.println("multiplication of two integer value:" + mult2);
+
+		System.out.println("enter two strings :\n");
+
+		String conct1 = ob.concat(sc.nextLine(),sc.nextLine());
+		System.out.println("concat of two string:" + conct1);
+
+		System.out.println("enter three strings :");
+		String conct2 = ob.concat(sc.nextLine(),sc.nextLine(),sc.nextLine());
+		System.out.println("concat of three string: " + conct2);
+	}
 }
