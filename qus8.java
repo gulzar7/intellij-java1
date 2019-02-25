bd4d41e3b645543b9ceacd933f3a1fb105deb30b
gitbootcamp <ahmad.gulzar8@gmail.com>
2019-02-25 16:48:26 +0530

    Update qus8.java

--- qus8.java
+++ qus8.java
@@ -1,8 +1,14 @@
 
+import java.util.*;
 public class qus8 {
 	public static void main(String[] args)
 	{
-	StringBuffer b1 = new StringBuffer("Welcome to TTN");
+		Scanner sc = new Scanner(System.in);
+
+
+		System.out.println("enter the string : ");
+		String value = sc.nextLine();
+	StringBuffer b1 = new StringBuffer(value);
 		System.out.println("String is:" +b1);
 		b1.reverse();
 		System.out.println("Reverse String :"+b1);
